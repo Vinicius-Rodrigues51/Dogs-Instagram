56003aaa4bf715f4a63c92cbbda06f31b9b903e7
Vinicius rodrigues <vinicius.motta.09@hotmail.com>
2022-01-28 20:57:20 -0300

    First commit

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Form/Input";
+import Button from "../Form/Button";
+import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+
+      <Link to="/login/criar">Criar</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
